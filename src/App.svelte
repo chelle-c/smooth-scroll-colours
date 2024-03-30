88b3d1b16e5426f2b457c47809b08788a4873249
Chelle <michellelcroke@gmail.com>
2024-03-30 18:46:14 -0230

    Various bug fixes
    - Removed unused code
    - Changed image alt text to match the correct image displayed
    - Changed calculations in background colour changing function to make colours update correctly based on scroll position
    - Made a separate function to call to make text white over darker sections (for better readability)
    - Added breakpoints to certain elements for mobile
    - Set body to fixed position to prevent address bar from hiding, breaking the web page (Chrome on mobile)

--- src/App.svelte
+++ src/App.svelte
@@ -5,13 +5,13 @@
 		{
 			id: 1,
 			image: 'red',
-			alt: 'A white building against a clear, light teal sky.',
+			alt: 'Exterior of a red tiled building with a window in the center covered with white blinds.',
 			colour: '#00c1b5',
 		},
 		{
 			id: 2,
 			image: 'blue',
-			alt: 'A window with a balcony railing on a house with an orange exterior. The railing holds three orange planting pots containing different succulents.',
+			alt: 'Exterior of a blue building.',
 			colour: '#ff651a',
 		},
 		{
@@ -23,13 +23,13 @@
 		{
 			id: 4,
 			image: 'orange',
-			alt: 'Exterior of a blue building.',
+			alt: 'A window with a balcony railing on a house with an orange exterior. The railing holds three orange planting pots containing different succulents.',
 			colour: '#1d3fbb',
 		},
 		{
 			id: 5,
 			image: 'turquoise',
-			alt: 'Exterior of a red tiled building with a window in the center covered with white blinds.',
+			alt: 'A white building against a clear, light teal sky.',
 			colour: '#e30512',
 		},
 	];
@@ -37,44 +37,31 @@
 	let box;
 	let yTop = 0;
 	let yBottom = 0;
-	let yHeight = 0;
 	let innerHeight = 0;
 	let lastTop = 0;
 	let lastBottom = 0;
 	let isScrolling = false;
+	let sectionIndex = 0;
 
 	const colours = sections.map(section => section.colour);
 	let bgColour = '#00c1b5';
 
 	let changeBGColour = () => {
-		isScrolling = true;
-		if (yTop >= 0 && yTop > lastTop) {
+		if (yTop >= 0 && yTop >= lastTop) {
 			// Scrolling down
 			for (let index = 1; index < colours.length; index++) {
-				if (yTop <= (innerHeight + 1) * index) {
+				if (yTop <= innerHeight * index) {
 					bgColour = colours[index];
-					index >= 3
-						? document
-								.getElementById('content')
-								.classList.add('text-white')
-						: document
-								.getElementById('content')
-								.classList.remove('text-white');
+					sectionIndex = index;
 					break;
 				}
 			}
-		} else if (yTop < lastTop) {
+		} else if (yTop <= lastTop) {
 			// Scrolling up
 			for (let index = 0; index < colours.length - 1; index++) {
-				if (lastTop < innerHeight * (index + 1)) {
+				if (yTop < (innerHeight) * (index + 1)) {
 					bgColour = colours[index];
-					index >= 3
-						? document
-								.getElementById('content')
-								.classList.add('text-white')
-						: document
-								.getElementById('content')
-								.classList.remove('text-white');
+					sectionIndex = index;
 					break;
 				}
 			}
@@ -87,9 +74,13 @@
 	let parseScroll = () => {
 		yTop = Math.floor(box.scrollTop);
 		yBottom = Math.floor(yTop + innerHeight);
-		yHeight = Math.floor(box.scrollHeight);
-		changeBGColour();
-		appHeight();
+	};
+
+	let whiteText = () => {
+		let content = document.getElementById('content');
+		sectionIndex >= 3
+			? content.classList.add('text-white')
+			: content.classList.remove('text-white');
 	};
 
 	const appHeight = () => {
@@ -106,72 +97,82 @@
 
 <div
 	id="background"
-	class="bgDisplay min-w-full min-h-full z-0 absolute"
-	style="transition: background-color 1.5s; background-color: {bgColour}"
+	class="absolute z-0 min-w-full min-h-full bgDisplay"
+	style="transition: background-color 1s; background-color: {bgColour}"
 />
 
 <main
 	id="content"
-	class="scrollsnap-container min-w-full min-h-full scroll-smooth"
+	class="min-w-full min-h-full scrollsnap-container scroll-smooth"
 	bind:this={box}
-	on:scroll={!isScrolling ? parseScroll : null}
+	on:scroll={() => {
+		if (!isScrolling) {
+			isScrolling = true;
+			parseScroll();
+			setTimeout(() => {
+				changeBGColour();
+				whiteText();
+			}, 100);
+		}
+	}}
 >
 	<div
-		class="min-w-full p-6 flex flex-row justify-between items-center z-10 absolute"
+		class="absolute z-10 flex flex-row items-center justify-between min-w-full p-1 md:p-3"
 	>
 		<div>
-			<h1 class="font-sans font-bold md:text-5xl text-3xl uppercase">
+			<h1
+				class="font-sans text-3xl font-bold uppercase content-text md:text-5xl"
+			>
 				Scroll & Colours
 			</h1>
 		</div>
-		<div class="font-sans font-bold text-xl">
+		<div class="font-sans font-bold content-text">
 			Made with Svelte and Tailwind CSS
 		</div>
 	</div>
 
 	{#each [...sections].reverse() as { id, image, alt }}
-		<section
-			id="section{id}"
-			class="[&:nth-child(5)]:text-white [&:nth-child(6)]:text-white"
-		>
+		<section id="section{id}">
 			<div class="drop-shadow-2xl mix-blend-hard-light">
 				<img
-					class="w-96 rounded-lg"
+					class="rounded-lg w-96 image"
 					src="images/image-{image}.jpg"
 					{alt}
 				/>
 			</div>
-			<h3 class="font-sans font-bold text-2xl p-6">Section #{id}</h3>
+			<h3 class="p-1 font-sans text-xl font-bold md:p-3">
+				Section #{id}
+			</h3>
 		</section>
 	{/each}
 
 	<div
-		class="min-w-full p-6 flex flex-row justify-between items-end z-100 absolute bottom-0 left-0 pointer-events-none"
+		class="absolute bottom-0 left-0 flex flex-row items-end justify-between min-w-full p-1 pointer-events-none md:p-3 z-100"
 	>
 		<div
-			class="font-sans font-semibold text-xl text-center underline pointer-events-auto hover:font-bold"
+			class="font-sans font-semibold text-center underline pointer-events-auto content-text hover:font-bold"
 		>
 			<a href="https://www.frontendpractice.com/projects/backstage-talks"
 				>Project Idea</a
 			>
 		</div>
 		<div
-			class="font-sans font-semibold text-xl text-center pointer-events-auto"
+			class="font-sans font-semibold text-center pointer-events-auto content-text"
 		>
 			<ul>
-				<li class="w-28">
+				<li class="md:w-28">
 					<a href="#section5" class="hover:font-bold">Section #5</a>
 				</li>
-				<li class="w-28">
+				<li class="md:w-28">
 					<a href="#section4" class="hover:font-bold">Section #4</a>
 				</li>
-				<li class="w-28">
+				<li class="md:w-28">
 					<a href="#section3" class="hover:font-bold">Section #3</a>
 				</li>
-				<li class="w-28">
+				<li class="md:w-28">
 					<a href="#section2" class="hover:font-bold">Section #2</a>
 				</li>
-				<li class="w-28">
+				<li class="md:w-28">
 					<a href="#section1" class="hover:font-bold">Section #1</a>
 				</li>
 			</ul>
